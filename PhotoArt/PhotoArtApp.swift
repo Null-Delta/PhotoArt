10fba517ce7a7e7af5a3dd9ad9698d0745c08fc5
Delta Null <delta.null@vk.com>
2022-10-09 22:21:54 +0300

    init project

---
+++ PhotoArt/PhotoArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoArtApp.swift
+//  PhotoArt
+//
+//  Created by Rustam Khakhuk on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
